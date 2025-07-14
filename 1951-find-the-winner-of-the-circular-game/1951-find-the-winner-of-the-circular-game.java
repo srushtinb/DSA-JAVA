d134a8d59b204b3cf1d2b00b31e048f40f7c8fc9
SRUSHTI NB <srushtinb08@gmail.com>
2025-07-14 10:28:01 +0530

    Time: 0 ms (100%), Space: 40.4 MB (88.72%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,14 +1,12 @@
+
 class Solution {
     public int findTheWinner(int n, int k) {
-        ArrayList<Integer>arr=new ArrayList<>();
-        for(int i=1;i<=n;i++)
-            arr.add(i);
-        int idx=0;
-        while(arr.size()>1){
-            idx=(idx+(k-1))%arr.size();
-            arr.remove(idx);
-
-        }
-        return arr.get(0);
+        int ind = helper(n,k);
+        return ind+1;
     }
+        int helper(int n,int k){
+            if(n==1)
+                return 0;
+            return (helper(n-1,k)+k)%n;
+        }
 }
