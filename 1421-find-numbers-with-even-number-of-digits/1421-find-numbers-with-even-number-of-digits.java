8fcb17ec5bf1b2f8d4d3f41915d1ecc46737d2e8
SRUSHTI NB <srushtinb08@gmail.com>
2025-07-14 21:20:59 +0530

    Time: 1 ms (98.65%), Space: 42.6 MB (95.31%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -17,14 +17,7 @@ class Solution {
         if(num<0){
             num=num* -1;
         }
-        if(num==0){
-            return -1;
+        return (int)(Math.log10(num))+1;
     }
-    int count=0;
-    while(num > 0){
-        count++;
-        num=num/10;
-    }
-    return count;
-}
+   
 }
